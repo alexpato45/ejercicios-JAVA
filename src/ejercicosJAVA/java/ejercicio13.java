0c52ca76e9e39968e5f39ee250590d9a614e2e73
yalitzademera <YALITZA DEMERA@YALITZA>
2014-07-03 17:56:52 -0600

    problema 13

---
+++ src/ejercicosJAVA/java/ejercicio13.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicosJAVA.java;
+
+/**
+ *
+ * @author YALITZA DEMERA
+ */
+public class ejercicio13 {
+    
+    public int[] Ordenar(int[] valores){
+    int respaldo[] = new int[valores.length];
+    int cont = 0;
+        for (int i = 0; i < valores.length; i++) {
+            if (valores[i] < 0) {
+                respaldo[cont] = valores[i];
+                cont++;
+                }
+        }
+        for (int i = 0; i < valores.length; i++) {
+            if (valores[i] >= 0) {
+                respaldo[cont] = valores[i];
+                cont++;
+                }
+        }
+        return respaldo;
+    }
+    
+}
